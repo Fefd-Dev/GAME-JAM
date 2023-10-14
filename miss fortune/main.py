7b0ebed2181362f1a8789053c46f41d155c7cd7f
Sondre Horpestad <sondreho@uia.no>
2023-10-14 04:52:57 +0200

    Mostly done

--- miss fortune/main.py
+++ miss fortune/main.py
@@ -4,43 +4,94 @@ import time
 import threading
 import keyboard
 
-# I want a charecter that can turn left and right and move by jumping difrent lengths by holding down the space bar
-# I also want to be able to jump on platforms to get to the end of the level
-# I want the platforms to move up and down
-
 # Global variables
 ## Player position can be read from anywhere because it is a array##
 global runing
 global looking_right
 global on_ground
 
-def on_ground_check():
-    '''Check if the player is on the ground'''
-    print('on_ground_check started')
+def create_platforms():
+    '''Create the platforms'''
+    print('create_platforms started')
+    global runing
+    
+    # Posisions of the platforms x and y
+    new_platform = [random.randint(0, WIDTH - PLAYER_SIZE), int(player_position[1]) - HEIGHT // 4, random.randint(200, 500)]
+    platforms.append(new_platform)
+    while runing:
+        if player_position[1] - new_platform[1] < 1000:
+            new_platform = [random.randint(max(0, new_platform[0]-900), min(WIDTH, new_platform[0]+900)), random.randint(new_platform[1]-450, new_platform[1]-200), random.randint(200, 500)]
+            platforms.append(new_platform)
+        
+        time.sleep(0.1)
+
+def on_platform_check():
+    '''Check if the player is on a platform'''
+    print('on_platform_check started')
     global on_ground
     global runing
-    while runing:
-        # TODO: make it so that the player can't go through objects
-        # TODO: make collision detection
-        if player_position[1] >= HEIGHT - PLAYER_SIZE:
-            # on_ground = True
-            time.sleep(0.01)
+    platform_id = 0
+    while runing:  
+        time.sleep(.01)
+        if player_position[1] + PLAYER_SIZE < platforms[platform_id][1] + platform_height:
+            platform_id += 1
+        elif platform_id > 0:
+            platform_id -= 1
+        
+        if not (player_position[0] <= platforms[platform_id][0] + platforms[platform_id][2] and player_position[0] + PLAYER_SIZE >= platforms[platform_id][0]):
+            continue
+        if (player_position[1] >= platforms[platform_id][1] + platform_height and player_position[1] <= platforms[platform_id][1] + platform_height*2):
+            player_position[1] = platforms[platform_id][1] + platform_height*2
+            player_position_update[1] = 1
+            on_ground = False
+            continue
+            
+        if not (player_position[1] + PLAYER_SIZE <= platforms[platform_id][1] + platform_height and player_position[1] + PLAYER_SIZE >= platforms[platform_id][1]):
+            continue
+        if not (player_position_update[1] > 0):
+            continue
+        
+        on_ground = True
+        player_position_update[0] = 1 
+        player_position_update[1] = 1
+        player_position[1] = platforms[platform_id][1] - PLAYER_SIZE
         
 def on_bottom_check():
     '''Check if the player is on the bottom'''
     global on_ground
+    gravety_force = 1
+    max_update = 20
+        
     if not on_ground:
         if player_position[1] < HEIGHT - PLAYER_SIZE:
-            player_position_update[1] += 1
-            player_position_update[1] = min(player_position_update[1], 30)
+            player_position_update[1] += gravety_force
+            player_position_update[1] = min(player_position_update[1], max_update)
         else:
             player_position_update[0] = 0
             player_position_update[1] = 0
             player_position[1] = HEIGHT - PLAYER_SIZE
             on_ground = True
+            
+def screen_scrool():
+    '''Screen scrolling vertically'''
+    print('screen_scrool started')
+    global runing
+    scrool_amount = 0
+    while runing:
+        time.sleep(0.01)
+        if player_position[1] < HEIGHT // 2 and on_ground:
+            player_position[1] += 20
+            scrool_amount += 20
+            for platform in platforms:
+                platform[1] += 20
+        if scrool_amount > 0 and player_position[1] > HEIGHT - HEIGHT // 3:
+            player_position[1] -= 20
+            scrool_amount -= 20
+            for platform in platforms:
+                platform[1] -= 20
 
 def update_position(object, object_speed):
-    """Update the position of an object"""
+    """Update the position of an player"""
     if on_ground:
         return
     for i in range(len(object)):
@@ -50,7 +101,6 @@ def update_position(object, object_speed):
         elif object[0] > WIDTH - PLAYER_SIZE:
             object[0] = WIDTH - PLAYER_SIZE
             object_speed[0] *= -1
-            
         object[i] += object_speed[i]
         
 def player_acceleration_update():
@@ -60,18 +110,22 @@ def player_acceleration_update():
         3. Limit the acceleration of the player
     '''
     global on_ground
-    
-    if not keyboard.is_pressed('space') or not on_ground or player_position_update[1] == -30:
-        on_ground = False
+    velocity_y = 0.15
+    velocity_x = 0.1
+    max_update_x = 16
+    max_update_y = 30
+    if not keyboard.is_pressed('space') or not on_ground or player_position_update[1] == -max_update_y:
+        if player_position_update[1] < 0:
+            on_ground = False
         return False
-    player_position_update[1] += -0.15
+    player_position_update[1] -= velocity_y
     if looking_right:
-        player_position_update[0] += 0.1
-        player_position_update[0] = min(max(player_position_update[0], 2), 16)
+        player_position_update[0] += velocity_x
+        player_position_update[0] = min(max(player_position_update[0], 2), max_update_x)
     else:
-        player_position_update[0] -= 0.1
-        player_position_update[0] = max(min(player_position_update[0], -2), -16)
-    player_position_update[1] = max(min(player_position_update[1], -4), -30)
+        player_position_update[0] -= velocity_x
+        player_position_update[0] = max(min(player_position_update[0], -2), -max_update_x)
+    player_position_update[1] = max(min(player_position_update[1], -4), -max_update_y)
     
     return True
     
@@ -113,7 +167,9 @@ player_position_update = [0,0]
 looking_right = True
 on_ground = True
 
-
+# Initialize platforms
+platform_height = 30
+platforms = []
 
 # Initialize pygame
 pygame.init()
@@ -126,7 +182,9 @@ pygame.display.set_caption("Get out of the well")
 # Starts threads
 threading.Thread(target=input_handler).start()
 threading.Thread(target=space_handler).start()
-threading.Thread(target=on_ground_check).start()
+threading.Thread(target=on_platform_check).start()
+threading.Thread(target=create_platforms).start()
+threading.Thread(target=screen_scrool).start()
 
 while runing:
     for event in pygame.event.get():
@@ -137,11 +195,14 @@ while runing:
     update_position(player_position, player_position_update)
     
     # Check if the player is on the bottom of the screen
-    on_bottom_check()
+    on_bottom_check()    
                 
-    # Fill the screen with white
+    # Clear the screen
     screen.fill((255, 255, 255))
-
+    
+    # draw the platforms
+    for platform in platforms:
+        pygame.draw.rect(screen, (0, 0, 0), (platform[0], platform[1], platform[2], platform_height))
     # Draw the player
     pygame.draw.rect(screen, (0, 0, 255), (player_position[0], player_position[1], PLAYER_SIZE, PLAYER_SIZE))
 
